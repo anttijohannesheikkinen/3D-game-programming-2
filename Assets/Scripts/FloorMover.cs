46be1d34014baacc9f7e5021c7444d01cc5fb829
Antti Heikkinen <antti.heikkinen@cs.tamk.fi>
2017-01-12 11:26:23 +0200

    Moving cubes.

---
+++ Assets/Scripts/FloorMover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMover : MonoBehaviour {
+
+
+
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public float duration;
+
+
+    private float totalTime;
+    private float startTime;
+    private float ratio;
+
+    private Transform ownTransform;
+
+	// Use this for initialization
+	void Awake () {
+        ownTransform = gameObject.GetComponent<Transform>();
+
+	}
+
+    void Start ()
+    {
+        startTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float ratio = (Time.time - startTime ) / duration;
+
+        if (ratio >= 1.0f) {
+            Destroy(gameObject);
+        }
+
+        ownTransform.position = Vector3.Lerp(startPosition, endPosition, ratio);
+
+    }
+}
