f4ebdee3cfa689930316048cea9aa0c43a68e1c0
Antti Heikkinen <antti.heikkinen@cs.tamk.fi>
2017-01-12 14:22:50 +0200

    Days more or less shit work.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    public CharacterController characterController;
+    public float movementSpeed;
+
+    void Awake ()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputX = Input.GetAxis("Horizontal");
+
+        Debug.Log(inputX);
+
+	}
+}
