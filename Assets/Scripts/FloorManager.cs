7186c9acbd08335edb6188750cc1c00f7223d7a1
Antti Heikkinen <antti.heikkinen@cs.tamk.fi>
2017-01-12 12:51:41 +0200

    A little broken floorManager because Lerp just isn't the best for this thing.

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -7,10 +7,12 @@ public class FloorManager : MonoBehaviour {
 
     public Vector3 startPosition;
     public Vector3 endPosition;
-    public float travelTime;
+    public float speed;
+    //public float travelTime;
     public float spawnFrequency = 1.0f;
-    //public int maxFloorTiles;
+    public int maxFloorTiles;
     public int floorTiles;
+
 
     private float timerStart;
 
@@ -18,20 +20,34 @@ public class FloorManager : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
+        float floorLength;
+
+        int amountOfInitialFloorTiles;
+        GameObject[] initialFloortiles = new GameObject[amountOfInitialFloorTiles];
+
+        foreach ()
+        {
+
+        }
+
         Spawn(Vector3.zero, new Vector3(0, 0, -10));
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if ((Time.time - timerStart >= spawnFrequency))
-        {
-            Spawn(startPosition, endPosition);
-        }
+        //if ((Time.time - timerStart >= spawnFrequency))
+        //{
+        //    Spawn(startPosition, endPosition);
+        //}
+
 	}
 
     public void FloorDestroyed ()
     {
         floorTiles--;
+        Spawn(startPosition, endPosition);
         //Instantiate(prefabs[Random.Range(0, prefabs.Length - 1 )]);
     }
 
@@ -43,7 +59,8 @@ public class FloorManager : MonoBehaviour {
         GameObject floorTile = Instantiate(prefabs[Random.Range(0, prefabs.Length - 1)]);
         FloorMover floorMover = floorTile.GetComponent<FloorMover>();
         //floorMover.floorManager = this;
-        floorMover.duration = travelTime;
+        //floorMover.duration = travelTime;
+        floorMover.speed = speed;
         floorMover.startPosition = startPos;
         floorMover.endPosition = endPos;
     }
--- Assets/Scripts/FloorMover.cs
+++ Assets/Scripts/FloorMover.cs
@@ -3,45 +3,56 @@ using System.Collections;
 
 public class FloorMover : MonoBehaviour {
 
-
+    public FloorManager floorManager;
 
     public Vector3 startPosition;
     public Vector3 endPosition;
+
+
+    //Lerp-based movement
     public float duration;
-
-    public FloorManager floorManager;
-
-
     private float totalTime;
     private float startTime;
     private float ratio;
 
     private Transform ownTransform;
 
-	// Use this for initialization
-	void Awake () {
+    // Velocity bsaed movement
+    private Vector3 direction;
+    public float speed;
+
+    // Use this for initialization
+    void Awake () {
         ownTransform = gameObject.GetComponent<Transform>();
         floorManager = GameObject.Find("FloorManager").GetComponent<FloorManager>();
-
 	}
 
     void Start ()
     {
-        startTime = Time.time;
+        //startTime = Time.time;
+        direction = (endPosition - startPosition).normalized;
         ownTransform.position = startPosition;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        float ratio = (Time.time - startTime ) / duration;
+        //float ratio = (Time.time - startTime ) / duration;
 
-        if (ratio >= 1.0f) {
+        //if (ratio >= 1.0f)
+        //{
+        //    floorManager.FloorDestroyed();
+        //    Destroy(gameObject);
+        //}
+
+        //ownTransform.position = Vector3.Lerp(startPosition, endPosition, ratio);
+
+        if (ownTransform.position.z < endPosition.z)
+        {
             floorManager.FloorDestroyed();
             Destroy(gameObject);
         }
 
-        ownTransform.position = Vector3.Lerp(startPosition, endPosition, ratio);
-
+        ownTransform.Translate(direction * speed * Time.deltaTime);
     }
 }
